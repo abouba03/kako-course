311ebdc10270c71131d7c53c76d87d1c09e75fe6
abouba03 <abouba1703@yandex.ru>
2023-10-14 22:11:47 +0300

    j'ai implimenter les data de l'API dans dossier pages->slug

--- kako-school/src/pages/products/[slug].js
+++ kako-school/src/pages/products/[slug].js
@@ -1,25 +1,58 @@
-export default function ProductPage() {}
+import Image from "next/image";
+import { supabase } from "supabase"
+
+export default function ProductPage({product}) {
+  console.log(product.slug)
+  const aa= product.slug
+  return(
+      <>
+            <section className="product-section">
+              <article className="product">
+                <div className="product-wrap">
+                  <Image src={`/assets/${product.slug}.png`} width={1000} height={300} alt={product.name} />
+                </div>
+                <section></section>
+              </article>
+            </section>
+      </>
+    )
+        
+  
+}
 
 
 export async function getStaticPaths() {
+
+  const {data: products} = await supabase.from("product").select("slug")
+
+  const paths = products.map((product) => ({
+    params: { slug: product.slug },
+  }));
+
+  console.log(paths)
+
+
     return {
-      paths: [
-        {
-          params: {
-            name: 'next.js',
-          },
-        }, // See the "paths" section below
-      ],
+      paths,// See the "paths" section below
       fallback: true, // false or "blocking"
     }
   }
    
-  export async function getStaticProps() {
-    const res = await fetch('https://api.github.com/repos/vercel/next.js')
-    const repo = await res.json()
-    return { props: { repo } }
+  export async function getStaticProps(context) {
+    const slug = context.params.slug
+
+    let { data: product } = await supabase
+  .from('product')
+  .select("*")
+  .eq("slug", slug)
+
+    return { 
+      props:{
+        product
+      }
+     }
   }
    
-  export default function Page({ repo }) {
-    return repo.stargazers_count
-  }
+  // export default function Page({ repo }) {
+  //   return repo.stargazers_count
+  // }
